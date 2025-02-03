c364f83e310202c8ca17fd90dd0ded69844e7291
Johan Smarius <jsmarius@hotmail.com>
2025-02-03 20:21:00 +0100

    Add project files.

---
+++ BlazorEventAppWebAssembly/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorEventAppWebAssembly.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Description { get; set; }
+
+        public EventType EventType { get; set; }
+    }
+}
---
+++ BlazorEventAppWebAssembly/Models/EventType.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorEventAppWebAssembly.Models
+{
+    public enum EventType
+    {
+        SPORTS,
+        MUSIC,
+        CONFERENCE,
+        CUlTURAL
+    }
+}
